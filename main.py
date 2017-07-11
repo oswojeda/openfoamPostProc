b819321f4efc3dd81a0f2845c5f2bad3b76e867d
Sebastian Hurtado <sebassdc@gmail.com>
2017-07-10 22:07:26 -0400

    links for work

---
+++ main.py
@@ -0,0 +1,3 @@
+http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+https://docs.python.org/2/library/os.path.html
+https://github.com/google/python-fire/blob/master/doc/guide.md
